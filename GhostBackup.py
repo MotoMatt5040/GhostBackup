cd0c4485374535170fe504ca394f5ad78373df24
motomatt5040 <motomatt5040@gmail.com>
2024-07-11 20:44:47 -0500

    feat(project): initial commit
    initial commit as a branch from another program.
    
    BREAKING CHANGE:

--- GhostBackup.py
+++ GhostBackup.py
@@ -1,151 +1,92 @@
+from dotenv import load_dotenv, set_key
+load_dotenv()
+
 import os
+import threading
+import time
 import shutil
 import msvcrt
 import tkinter as tk
 from tkinter import filedialog
 
-def check_dir():
-    """
-    Change working dir to savegame directory if savegamedir.txt is defined,
-    else ask for the savegame directory with GUI.
+threads = []
 
-    Parameters
-    ----------
-    None.
+if not os.environ.get('save_game_dir'):
+    print('Running initial setup script:\n\nPlease select your save game directory.\n    --NOTE: This is the folder containing your save folders, not your single instance save folder.')
+    save_path = filedialog.askdirectory()
+    set_key('.env', 'save_game_dir', save_path)
 
-    Returns
-    ----------
-    None.
-    """
+    allowed_saves_count = input('Please enter the amount of backup files you would like to be saved.')
+    set_key('.env', 'allowed_saves_count', allowed_saves_count)
 
-    try:
-        if os.path.isfile("savegamedir.txt"):
-            with open("savegamedir.txt", 'r') as f:
-                path = f.readlines()[0]
-        else:
-            with open("savegamedir.txt", 'w+') as f:
-                print("Select your GRW save game directory")
-                window = tk.Tk()
-                window.withdraw()
-                path = filedialog.askdirectory()
-                f.write(path)
-        os.chdir(path)
-    except Exception as e:
-        print("There was an issue with savegame path. Remove the savegamedir.txt and try again.")
-        print(e)
+    input('Setup complete, please close and run script again to save state.')
 
-    return
 
-def backup():
-    """
-    Backup the save and move each backup slot forward one notch until backup 5.
-    Savegame -> backup 1
-    backup 1 -> backup 2
-    backup 2 -> backup 3 and so on, the oldest version is removed.
+def reset() -> None:
+    print('Running initial setup script:\n\nPlease select your save game directory.\n    --NOTE: This is the folder containing your save folders, not your single instance save folder.')
+    save_path = filedialog.askdirectory()
+    set_key('.env', 'save_game_dir', save_path)
 
-    Parameters
-    ----------
-    None.
+    allowed_saves_count = input('Please enter the amount of backup files you would like to be saved.')
+    set_key('.env', 'allowed_saves_count', allowed_saves_count)
 
-    Returns
-    ----------
-    None.
-    """
-    
-    savedir = "3559"
-    tempdir = "3559 - temp"
+    input('Setup complete, please close and run script again to save state.')
 
-    for i in range(5, 0, -1):
-        backupdir = backupdir = "3559 - backup " + str(i)
-        prev_backup = "3559 - backup " + str(i - 1)
 
-        if not os.path.exists(prev_backup) and (i != 1):
-            continue
-        if os.path.exists(backupdir):
-            shutil.rmtree(backupdir)
-        if os.path.exists(tempdir):
-            shutil.rmtree(tempdir)
-        
-        if i == 1:
-            shutil.copytree(savedir, tempdir)
-        else:
-            shutil.copytree(prev_backup, tempdir)
-            
-        os.rename(tempdir, backupdir)
+def backup() -> None:
+    while True:
+        save_path = os.environ.get('save_game_dir')
+        saves = os.listdir(save_path)
 
-    input("Backup successful, press enter to exit.")
+        if len(saves) > int(os.environ['allowed_saves_count']):
+            os.mkdir(os.path.join(save_path, 'temp'))
+            shutil.copytree(f'{save_path}/{saves[0]}', f'{os.path.dirname(save_path)}/temp/{saves[0]}')
+            # save_path = f"{os.path.dirname(save_path)}/temp"
+
+        target_backup = saves[-1]
+        os.rename(f'{save_path}/{target_backup}', f"{save_path}/{target_backup.replace('-latest', f'-{len(saves)}')}")
+        target_backup = target_backup.replace('-latest', f'-{len(saves)}')
+        shutil.copytree(f'{save_path}/{target_backup}', f'{save_path}/{target_backup.replace(f"{len(saves)}", "latest")}')
+        time.sleep(300)
 
 
 def restore():
-    """
-    Restore savegame from defined backup number.
+    backup_file_path = filedialog.askdirectory()
 
-    Parameters
-    ----------
-    None.
+    saves = os.environ['save_game_dir']
+    print(os.listdir(saves))
 
-    Returns
-    ----------
-    None.
-    """
-    
-    n_backups = len([x for x in os.listdir() if x.startswith("3559 - backup")])
-
-    if n_backups == 0:
-        input("No backups found. Press enter to exit.")
-
-    while True:
-        inp = input("Which save to restore (1-{})? ".format(n_backups))
-        backupdir = backupdir = "3559 - backup " + inp
-
-        if os.path.exists(backupdir):
-            break
-        else:
-            print("Invalid backup number.")    
-
-    if os.path.exists("3559"):
-        shutil.rmtree("3559")
-    shutil.copytree(backupdir, "3559")
-
-    input("Savefiles successfully restored from backup {}, press enter to exit.".format(inp))
 
 def interface():
-    """
-    Run the command line interface for the program.
-
-    Parameters
-    ----------
-    None.
-
-    Returns
-    ----------
-    None.
-    """
-
+    print('Available commands.')
+    print('p     reset save path')
+    print('r     restore latest save')
     while True:
-        print("Press B to backup or R to restore your Ghost Recon Wildlands savefile.")
-        input_char = msvcrt.getch().decode()
-        
-        if input_char.lower() == "b":
-            print("Confirm save BACKUP by pressing Y or return with any key.")
-            if msvcrt.getch().decode().lower() == 'y':
-                backup()
-                break
-            else:
-                print("Savefile not backed up.")
-                continue
-        
-        elif input_char.lower() == "r":
-            print("Confirm save RESTORE by typing Y or return with any key.")
-            if msvcrt.getch().decode().lower() == 'y':
-                restore()
-                break
-            else:
-                print("Savefile not restored.")
-                continue
-        else:
-            print("Invalid option.")
+        try:
+            option = input().lower()
+            match option:
+                case 'p':
+                    reset()
+                case 'b':
+                    backup()
+                case 'r':
+                    restore()
+                case _:
+                    print('Invalid option.')
+        except:
+            print('Invalid option.')
 
 if __name__ == "__main__":
-    check_dir()
-    interface()
+    threads.append(threading.Thread(target=backup))
+    threads.append(threading.Thread(target=interface))
+
+    for thread in threads:
+        thread.start()
+
+
+
+
+
+    # backup()
+    # interface()
+    # restore()
