6a8046bb0221c4de52f12d407924b31fe1ab45fe
motomatt5040 <motomatt5040@gmail.com>
2024-07-11 22:52:28 -0500

    fix(GhostBackup.py): fix save interval, backup, restore
    Fixed a bug where the save interval was not properly setting in the environment variable. This was due to it being a string rather than an integer. Fixed a bug where save files were only creating new files instead of backing up the actual save file. Fixed a bug where restore was not properly restoring from selected file.

--- GhostBackup.py
+++ GhostBackup.py
@@ -18,11 +18,18 @@ def reset() -> None:
     save_path = filedialog.askdirectory()
     set_key('.env', 'save_game_dir', save_path)
 
-    allowed_saves_count = input('Please enter the amount of backup files you would like to be saved.')
+    allowed_saves_count = input('Please enter the amount of backup files you would like to be saved: ')
     set_key('.env', 'allowed_saves_count', allowed_saves_count)
 
-    save_interval = input('Please enter the amount of time between backups in minutes: ')
-    set_key('.env', 'save_interval', save_interval * 60)
+    print('Please enter the amount of time between backups in minutes:\n')
+    while True:
+        try:
+            save_interval = int(input()) * 60
+            break
+        except:
+            print('Please enter a valid number.')
+
+    set_key('.env', 'save_interval', str(save_interval))
 
     input('Setup complete, please close and run script again to save state.')
 
@@ -47,46 +54,61 @@ def backup() -> None:
         saves = os.listdir(save_path)
         saves = sorted(saves, key=lambda x: (extract_number(x), x))
 
-        if len(saves) > int(allowed_saves_count):
-            shutil.rmtree(os.path.join(save_path, saves[1]))
-            for i in range(1, int(allowed_saves_count)):
-                file_to_change = os.path.join(save_path, saves[i + 1])
-                name_to_change_to = os.path.join(save_path, saves[i])
-                os.rename(file_to_change, name_to_change_to)
+        amount_to_delete = len(saves) - int(allowed_saves_count)
+
+        if amount_to_delete > 0:
+            for i in range(1, amount_to_delete + 1):  # +1 is used here so that -latest will be the finaly entry in the count
+                shutil.rmtree(os.path.join(save_path, saves[i]))
+                # print(f"Deleted: {os.path.join(save_path, saves[i])}")
+
+        remaining_saves = os.listdir(save_path)
+        remaining_saves = sorted(remaining_saves, key=lambda x: (extract_number(x), x))
+
+        new_save_index = 2
+        for save in remaining_saves:
+            if save == 'Save' or save == 'Save-latest':
+                continue
+            new_name = f'Save-{new_save_index}'
+            old_path = os.path.join(save_path, save)
+            new_path = os.path.join(save_path, new_name)
+            os.rename(old_path, new_path)
+            # print(f"Renamed: {old_path} to {new_path}")
+            new_save_index += 1
 
         saves = os.listdir(save_path)
         saves = sorted(saves, key=lambda x: (extract_number(x), x))
-        print(saves)
-        target_backup = saves[-1]
+        # print(saves)
+        target_backup_name = saves[-1]
 
-        target_file = f'{save_path}/{target_backup}'
-        target_rename = f"{save_path}/{target_backup.replace('-latest', f'-{len(saves)}')}"
+        target_file = f'{save_path}/{target_backup_name}'
+        target_rename = f"{save_path}/{target_backup_name.replace('-latest', f'-{len(saves)}')}"
         os.rename(target_file, target_rename)
 
-        target_backup = target_backup.replace('-latest', f'-{len(saves)}')
+        target_backup_name = target_backup_name.replace('-latest', f'-{len(saves)}')
 
-        target_file = f'{save_path}/{target_backup}'
-        target_rename = f'{save_path}/{target_backup.replace(f"{len(saves)}", "latest")}'
+        target_file = f'{save_path}/{saves[0]}'
+        target_rename = f'{save_path}/{target_backup_name.replace(f"{len(saves)}", "latest")}'
         shutil.copytree(target_file, target_rename)
 
-        save_interval = float(os.environ.get('save_interval'))
+        save_interval = os.environ.get('save_interval')
         if not save_interval:
             time.sleep(900)
             continue
-        time.sleep(save_interval)
+        time.sleep(float(save_interval))
 
 
 def restore(restore_type=None) -> None:
     save_path = os.environ['save_game_dir']
     saves = os.listdir(save_path)
+    original_save_dir = saves[0]
+    shutil.rmtree(f'{save_path}/{original_save_dir}')
     if not restore_type:
-        backup_file_path = saves[-1]
-        original_save_dir = saves[0]
+        backup_save_dir = saves[-1]
+        shutil.copytree(f'{save_path}/{backup_save_dir}', f'{save_path}/{original_save_dir}')
     else:
-        backup_file_path = filedialog.askdirectory(title="Select backup to restore to.")
-        original_save_dir = saves[0]
-    shutil.rmtree(original_save_dir)
-    shutil.copytree(f'{backup_file_path}', f'{original_save_dir}')
+        backup_save_dir = filedialog.askdirectory(title="Select backup save game to restore.")
+        shutil.copytree(f'{backup_save_dir}', f'{save_path}/{original_save_dir}')
+
     print(f'Restore complete.')
 
 
@@ -100,17 +122,21 @@ def interface():
             match option:
                 case 'p':
                     reset()
+                    exit()
                 case 'r':
-                    restore_type = input("Press enter for latest, or type 'select' to select a specific backup")
+                    restore_type = input("Press enter for latest, or type 'select' to select a specific backup:\n\n")
                     restore(restore_type)
                 case _:
                     print('Invalid option.')
         except:
+            import traceback
+            print(traceback.format_exc())
             print('Invalid option.')
 
 
 if not os.environ.get('save_game_dir'):
     reset()
+    exit()
 
 if __name__ == "__main__":
     threads.append(threading.Thread(target=backup))
